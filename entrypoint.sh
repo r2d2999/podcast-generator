da545988ce0ac690d27f42ad4234d62c87a86388
r2d2999 <41614964+r2d2999@users.noreply.github.com>
2024-06-29 07:00:35 +0000

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+set -e
 
 echo "===================="
-
+echo "Configurando Git"
 git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
-python3 /urs/bin/feed.py
+echo "Ejecutando feed.py"
+python3 /usr/bin/feed.py
 
-git add -A && git commit -m "upadted feed"
+echo "Añadiendo cambios y haciendo commit"
+git add -A
+git commit -m "updated feed"
 git push --set-upstream origin main
-
 
 echo "===================="
